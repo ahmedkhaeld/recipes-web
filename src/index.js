56fcd21c59b22ff374674a246e7d67140761c3ee
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-06-10 00:57:57 +0200

    create a dashboard to consume recipes-api with user logging in and logout by Auth0

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+      <Auth0Provider
+          domain="AUTH0_DOMAIN"
+          clientId="AUTH_CLIENT_ID"
+          redirectUri={window.location.origin}
+      >
+          <App />
+      </Auth0Provider>,
+
 );
 
 // If you want to start measuring performance in your app, pass a function
